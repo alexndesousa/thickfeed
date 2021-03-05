0c6f3ce0e922fb015022d8533ac9c55df6722c70
Alex De Sousa <alexndesousa@hotmail.co.uk>
2021-03-05 11:33:36 +0000

    Documented getTrending function

--- src/services/youtube_service.js
+++ src/services/youtube_service.js
@@ -4,6 +4,13 @@ const fs = require('fs')
 // Loads the youtube API key into memory
 const apiKey = fs.readFileSync('../keys/youtube_api_key.txt', 'utf-8')
 
+/**
+ * Retrieve trending videos for a given country
+ * @param {string} countryCode - The country to receive trending videos for
+ * @param {string} page - Used for pagination
+ * @param {number} limit - The max number of videos to retrieve
+ * @returns A promised JSON response
+ */
 const getTrending = (countryCode = 'GB', page = '', limit = 10) => {
     const baseUrl = 'https://youtube.googleapis.com/youtube/v3/videos'
     return fetch(`${baseUrl}?part=player&chart=mostPopular&regionCode=${countryCode}&key=${apiKey}&maxResults=${limit}&pageToken=${page}`)
