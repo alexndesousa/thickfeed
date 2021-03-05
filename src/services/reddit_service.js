739c738a411a1d76cf35dd81e6f411643b3abe52
Alex De Sousa <alexndesousa@hotmail.co.uk>
2021-03-05 10:37:37 +0000

    Added documentation + getHot now returns response

--- src/services/reddit_service.js
+++ src/services/reddit_service.js
@@ -1,9 +1,15 @@
 const fetch = require('node-fetch');
 
+/**
+ * Retrieve hot posts in the given subreddit
+ * @param {string} subreddit - The subreddit to retrieve hot posts from
+ * @param {string} after - Used for retrieving the next page
+ * @param {number} limit - The maximum number of posts to retrieve
+ * @returns A promised JSON response
+ */
 const getHot = (subreddit = 'all', after = '', limit = 10) => {
-    fetch(`https://www.reddit.com/r/${subreddit}/hot.json?after=${after}&limit=${limit}`)
-        .then(res => res.text())
-        .then(body => console.log(body))
+    return fetch(`https://www.reddit.com/r/${subreddit}/hot.json?after=${after}&limit=${limit}`)
+        .then(res => res.json())
 }
 
 module.exports = {
