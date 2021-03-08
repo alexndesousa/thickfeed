b11f26c1594c34a614a543df549c21da4c75e39b
Alex De Sousa <alexndesousa@hotmail.co.uk>
2021-03-08 22:26:36 +0000

    Implemented method to retrieve woeid

--- src/services/twitter_service.js
+++ src/services/twitter_service.js
@@ -10,12 +10,36 @@ const getTrendingTopics = (countryWOEID = 1) => {
     const parameterisedUrl = `${baseUrl}?id=${countryWOEID}`
     const bearerToken = process.env.TWITTER_BEARER_TOKEN
     const options = {
-        method = 'GET',
         headers: {'Authorization': `Bearer ${bearerToken}`}
     }
 
     return fetch(parameterisedUrl, options)
     .then(res => res.json())
+}
+
+
+const getWOEID = (countryCode = null) => {
+    // if no countryCode is provided we'll provide the worldwide woeid
+    if (countryCode === null) {
+        return 1;
+    }
+
+    const baseUrl = 'https://api.twitter.com/1.1/trends/available.json'
+    const bearerToken = process.env.TWITTER_BEARER_TOKEN
+    const options = {
+        headers: {'Authorization': `Bearer ${bearerToken}`}
+    }
+
+    return fetch(baseUrl, options)
+    .then(res => res.json())
+    .then(body => {
+        for (i=0; i<body.length; i++) {
+            if (body[i].countryCode == countryCode) {
+                console.log(body[i].parentid)
+                return body[i].parentid;
+            }
+        }
+    })
 }
 
 module.exports = {
