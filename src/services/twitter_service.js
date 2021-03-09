e9b4ebfbb6a1244a6f91187fa1d771546c27aed0
Alex De Sousa <alexndesousa@hotmail.co.uk>
2021-03-09 20:40:44 +0000

    Implemented searching for tweets + some cleanup

--- src/services/twitter_service.js
+++ src/services/twitter_service.js
@@ -20,7 +20,7 @@ const getTrendingTopics = (countryWOEID = 1) => {
 /**
  * Retrieve the WOEID for the given country code
  * @param {string} countryCode - country code to find a WOEID for
- * @returns A promised string
+ * @returns A promised integer
  */
 const getWOEID = (countryCode = null) => {
     // if no countryCode is provided we'll provide the worldwide woeid
@@ -46,6 +46,29 @@ const getWOEID = (countryCode = null) => {
     })
 }
 
+/**
+ * Retrieve tweets that match the given search term
+ * @param {string} searchTerm - What we want to search for, will typically be a hashtag
+ * @param {string} type - The type of results we want to show; popular or recent or mixed
+ * @param {number} limit - The max number of tweets to retrieve
+ * @returns A promised JSON response
+ */
+const searchTweets = (searchTerm, type = 'popular', limit = 10) => {
+    const encodedSearchTerm = encodeURI(searchTerm)
+
+    const baseUrl = 'https://api.twitter.com/1.1/search/tweets.json'
+    const parameterisedUrl = `${baseUrl}?q=${encodedSearchTerm}&result_type=${type}&count=${limit}`
+    const bearerToken = process.env.TWITTER_BEARER_TOKEN
+    const options = {
+        headers: {'Authorization': `Bearer ${bearerToken}`}
+    }
+
+    return fetch(parameterisedUrl, options)
+    .then(res => res.json())
+}
+
 module.exports = {
-    getTrendingTopics
+    getTrendingTopics,
+    getWOEID,
+    searchTweets
 }
