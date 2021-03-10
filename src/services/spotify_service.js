0725dd47e60e6e43c9e5b5b516a9145239fd4c7c
Alex De Sousa <alexndesousa@hotmail.co.uk>
2021-03-10 23:45:47 +0000

    Added retrieving new releases for a given country

--- src/services/spotify_service.js
+++ src/services/spotify_service.js
@@ -1,8 +1,8 @@
 const fetch = require('node-fetch');
 const { URLSearchParams } = require('url');
 /**
- *
- * @returns
+ * Retrieve a client bearer access token to authorize requests to spotify
+ * @returns A promised string
  */
 const getAccessToken = () => {
   const clientId = process.env.SPOTIFY_CLIENT_ID;
@@ -22,9 +22,35 @@ const getAccessToken = () => {
       Authorization: `Basic ${encodedIdAndSecret}`,
     },
   };
+
   return fetch(baseUrl, options)
     .then((res) => res.json())
     .then((res) => res.access_token);
 };
 
-getAccessToken();
+/**
+ * Retrieve the new releases in a given country
+ * @param {string} countryCode - The country for which we want to find new releases for
+ * @param {number} offset - The amount we want to offset the results by; used for pagination
+ * @param {number} limit - The maximum amount of releases we want to retreive
+ * @param {*} accessToken - The access token to authenticate the request
+ * @returns A promised JSON response
+ */
+const getNewReleases = (countryCode = 'GB', offset = 0, limit = 10, accessToken) => {
+  const baseUrl = 'https://api.spotify.com/v1/browse/new-releases';
+  const parameterisedUrl = `${baseUrl}?country=${countryCode}&limit=${limit}&offset=${offset}`;
+
+  const options = {
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+    },
+  };
+
+  return fetch(parameterisedUrl, options)
+    .then((res) => res.json());
+};
+
+module.exports = {
+  getAccessToken,
+  getNewReleases,
+};
