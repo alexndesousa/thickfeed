2abd4721ba15aac803183b6303f73786afd50e80
Alex De Sousa <alexndesousa@hotmail.co.uk>
2021-03-26 18:47:26 +0000

    Implemented cors middleware

--- thickfeed-apis/app.js
+++ thickfeed-apis/app.js
@@ -1,9 +1,16 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const { fetchAndStoreFeeds, getFeed } = require('./src/controllers/trending_feed_controller');
 
 const app = express();
 const port = 3001;
+
+const corsOptions = {
+  origin: 'http://localhost',
+};
+
+app.use(cors(corsOptions));
 
 app.get('/test', getFeed);
 
