a80265167bf370d38a575cf40bfba02b84a758e6
Alex De Sousa <alexndesousa@hotmail.co.uk>
2021-04-02 11:47:56 +0100

    UI now shows the feed on screen

--- thickfeed-ui/src/App.tsx
+++ thickfeed-ui/src/App.tsx
@@ -1,15 +1,34 @@
-import React from 'react';
+import * as React from 'react';
+import useScript from './hooks/useScript';
 import logo from './logo.svg';
 import './App.css';
+import { getFeed } from './services/feedService';
 
-function App() {
+const App = (): JSX.Element => {
+  const [feed, setFeed] = React.useState<Array<JSX.Element>>([]);
+  const [offset] = React.useState(0);
+  const [feedOptions] = React.useState({
+    offset,
+    limit: 40,
+    spotify: true,
+    reddit: true,
+    twitter: true,
+    youtube: true,
+    bbc: true,
+  });
+  React.useEffect(() => {
+    getFeed(feedOptions, 500, 500)
+      .then((newFeed) => setFeed(newFeed));
+  }, []);
+  // React.useEffect(() => {
+  //   console.log(feed);
+  // }, [feed]);
+  useScript('https://embed.redditmedia.com/widgets/platform.js');
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        {feed}
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -21,6 +40,5 @@ function App() {
       </header>
     </div>
   );
-}
-
+};
 export default App;
