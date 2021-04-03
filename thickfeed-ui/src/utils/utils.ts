7154199154efcf9986b9692a3431daacdade7b59
Alex De Sousa <alexndesousa@hotmail.co.uk>
2021-04-03 18:07:38 +0100

    Added function to format unix epoch into time unit

--- thickfeed-ui/src/utils/utils.ts
+++ thickfeed-ui/src/utils/utils.ts
@@ -3,7 +3,7 @@
  * @param {Array<string>} array - The array to be shuffled
  * @returns An array of strings
  */
-const shuffle = (array: Array<JSX.Element>): Array<JSX.Element> => {
+export const shuffle = (array: Array<JSX.Element>): Array<JSX.Element> => {
   const arr: Array<JSX.Element> = array;
   let currentIndex = arr.length;
   let temporaryValue;
@@ -22,4 +22,23 @@ const shuffle = (array: Array<JSX.Element>): Array<JSX.Element> => {
   return arr;
 };
 
-export default shuffle;
+export const formatUTC = (utc: number): string => {
+  let quotient = Math.floor(utc / 86400);
+  let unit = 'day';
+  if (utc < 60) {
+    quotient = utc;
+    unit = 'second';
+  }
+  if (utc < 3600) {
+    quotient = Math.floor(utc / 60);
+    unit = 'minute';
+  }
+  if (utc < 86400) {
+    quotient = Math.floor(utc / 3600);
+    unit = 'hour';
+  }
+  if (quotient > 1) {
+    unit = `${unit}s`;
+  }
+  return `${quotient} ${unit} ago`;
+};
