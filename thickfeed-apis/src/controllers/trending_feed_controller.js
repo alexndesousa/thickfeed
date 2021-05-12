428a96cf8b97837ee68a0aad2345ad4efc902a77
Alex De Sousa <alexndesousa@hotmail.co.uk>
2021-05-12 17:34:02 +0100

    fresh data now fetched every 60 seconds

--- thickfeed-apis/src/controllers/trending_feed_controller.js
+++ thickfeed-apis/src/controllers/trending_feed_controller.js
@@ -91,7 +91,7 @@ const fetchAndStoreFeeds = async () => {
     await generateTrendingFeed(options);
 
     // delay for retrieving next batch of feed elements
-    await new Promise((resolve) => setTimeout(resolve, 1000000));
+    await new Promise((resolve) => setTimeout(resolve, 60000));
   }
 };
 
